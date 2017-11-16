798ff01bb617fa66e93ccff63f2cb6c882fe57af
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-16 12:35:57 +0200

    InitialDesignPlusAppendCommand

---
+++ ProcessingListOfStrings/Commands/AppendCommand.cs
@@ -0,0 +1,27 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingListOfStrings.Commands
+{
+    public class AppendCommand : ICommand
+    {
+        public void Execute(List<string> listToProcess, List<string> inputArgs)
+        {
+            if (inputArgs.Count != 2)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+            }
+            else
+            {
+                string stringToAppend = inputArgs[1];
+                listToProcess.Add(stringToAppend);
+                List<string> result = listToProcess;
+                Console.WriteLine(String.Join(" ", result));
+            }
+        }
+    }
+}
---
+++ ProcessingListOfStrings/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+
+namespace ProcessingListOfStrings
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> initialList = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Console.WriteLine(String.Join(" ", initialList));
+
+            //If you want to add a new command, add it here as well!
+            List<string> allowedCommands = new List<string> { "append", "prepend", "reverse", "insert", "delete", "roll"
+            , "sort", "count", "end"};
+
+            string commandLine = Console.ReadLine();
+
+            while (true)
+            {
+                List<string> args = commandLine.Split(' ').ToList();
+                string command = args[0];
+                if (!allowedCommands.Contains(command))
+                {
+                    Console.WriteLine("Error: invalid command");
+                    commandLine = Console.ReadLine();
+                    continue;
+                }
+                string concreteCommandName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(command) + "Command";
+                Type commandType = Type.GetType($"ProcessingListOfStrings.Commands.{concreteCommandName}");
+                ICommand concreteCommand = (ICommand)Activator.CreateInstance(commandType);
+                concreteCommand.Execute(initialList, args);
+                if (command == "end" && args.Count == 1)
+                {
+                    break;
+                }
+                commandLine = Console.ReadLine();
+            }
+
+        }
+    }
+}
