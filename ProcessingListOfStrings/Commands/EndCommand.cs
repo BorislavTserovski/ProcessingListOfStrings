15c9f30bb4bae1c4ae2aadaf39d3f85d64349030
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-16 13:07:20 +0200

    Done

---
+++ ProcessingListOfStrings/Commands/EndCommand.cs
@@ -0,0 +1,22 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+
+
+namespace ProcessingListOfStrings.Commands
+{
+    public class EndCommand : ICommand
+    {
+        public void Execute(List<string> listToProcess, List<string> inputArgs)
+        {
+            if (inputArgs.Count != 1)
+            {
+                Console.WriteLine("Ërror: invalid command parameters");
+            }
+            else
+            {
+                Console.WriteLine("Finished");
+            }
+        }
+    }
+}
---
+++ ProcessingListOfStrings/Commands/RollCommand.cs
@@ -0,0 +1,40 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ProcessingListOfStrings.Commands
+{
+    public class RollCommand : ICommand
+    {
+        public void Execute(List<string> listToProcess, List<string> inputArgs)
+        {
+            if (inputArgs.Count != 2)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+            }
+            else
+            {
+                if (inputArgs[1] == "left")
+                {
+                    string first = listToProcess[0];
+                    listToProcess.RemoveAt(0);
+                    listToProcess.Add(first);
+                    Console.WriteLine(String.Join(" ", listToProcess));
+                }
+                else if (inputArgs[1] == "right")
+                {
+                    string last = listToProcess.Last();
+                    listToProcess.RemoveAt(listToProcess.Count - 1);
+                    listToProcess.Insert(0, last);
+                    Console.WriteLine(String.Join(" ", listToProcess));
+                }
+                else
+                {
+                    Console.WriteLine("Error: invalid command parameters");
+                }
+            }
+        }
+    }
+}
