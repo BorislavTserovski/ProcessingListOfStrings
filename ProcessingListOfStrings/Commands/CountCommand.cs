87336d93d9d342288741ff7b64591544c02bda15
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-16 13:03:43 +0200

    SortAndCountCommands

---
+++ ProcessingListOfStrings/Commands/CountCommand.cs
@@ -0,0 +1,33 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+
+
+namespace ProcessingListOfStrings.Commands
+{
+
+    public class CountCommand : ICommand
+    {
+        public void Execute(List<string> listToProcess, List<string> inputArgs)
+        {
+            if (inputArgs.Count != 2)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+            }
+            else
+            {
+                string searchWord = inputArgs[1];
+                int counter = 0;
+
+                foreach (var word in listToProcess)
+                {
+                    if (word == searchWord)
+                    {
+                        counter++;
+                    }
+                }
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
--- ProcessingListOfStrings/Commands/SortCommand.cs
+++ ProcessingListOfStrings/Commands/SortCommand.cs
@@ -5,17 +5,17 @@ using System.Collections.Generic;
 
 namespace ProcessingListOfStrings.Commands
 {
-    public class ReverseCommand : ICommand
+    public class SortCommand : ICommand
     {
         public void Execute(List<string> listToProcess, List<string> inputArgs)
         {
             if (inputArgs.Count != 1)
             {
-                Console.WriteLine("Error: invalid command parameters");
+                Console.WriteLine("Error invalid command parameters");
             }
             else
             {
-                listToProcess.Reverse();
+                listToProcess.Sort();
                 List<string> result = listToProcess;
                 Console.WriteLine(String.Join(" ", result));
             }
