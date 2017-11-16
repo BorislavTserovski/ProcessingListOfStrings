60423f1e5aa15bacb926a62ea27d4526d0097901
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-16 13:01:04 +0200

    InsertAndDeleteCommands

---
+++ ProcessingListOfStrings/Commands/DeleteCommand.cs
@@ -0,0 +1,41 @@
+﻿using ProcessingListOfStrings.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingListOfStrings.Commands
+{
+    public class DeleteCommand : ICommand
+    {
+        public void Execute(List<string> listToProcess, List<string> inputArgs)
+        {
+            if (inputArgs.Count != 2)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+            }
+            else
+            {
+                int index = -1;
+                bool isNumber = int.TryParse(inputArgs[1], out index);
+                if (!isNumber)
+                {
+                    Console.WriteLine($"Error: invalid command parameters");
+                }
+                else
+                {
+                    if (listToProcess.ElementAtOrDefault(index) != null)
+                    {
+                        listToProcess.RemoveAt(index);
+                        Console.WriteLine(String.Join(" ", listToProcess));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: invalid index {inputArgs[1]}");
+                    }
+                }
+            }
+        }
+    }
+}
